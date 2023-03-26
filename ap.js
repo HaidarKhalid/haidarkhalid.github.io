5480523207041dbe88289b9fa92349b755bf48dd
Haidar Khalid <110727504+HaidarKhalid@users.noreply.github.com>
2023-03-26 17:11:50 +0300

    Update ap.js

--- ap.js
+++ ap.js
@@ -1,5 +1,6 @@
 
 let myProjects = {
+    "WeatherHere app" : {src:"images/projects/weatherhere-app.png",link:"https://haidarkhalid.github.io/WeatherHere/"},
     "DuckDuckGo recreate":{src:"images/projects/DuckDuckGo-recreate.png",link:"https://haidarkhalid.github.io/DuckDuckGo-recreate/"},
     "Cat vs Dogs":{src:"images/projects/cat-vs-dogs.png",link:"https://haidarkhalid.github.io/cat-vs-dogs/"},
     "Blackjack game":{src:"images/projects/BlackJack.png",link:"https://haidarkhalid.github.io/blackjack-game/"},
@@ -13,7 +14,7 @@ let myProjects = {
     "Digital clock": {src:"images/projects/digital-clock.png",link:"https://haidarkhalid.github.io/Digital-clock/"},
     "Colors game": {src:"images/projects/colors-game.png",link:"https://haidarkhalid.github.io/colors-game/"},
 }
-let projectNames = ["DuckDuckGo recreate","Cat vs Dogs","Blackjack game","Score counter","Charecters counter","Multiplication quiz","Calculator","Gaming quiz","Password generator","To-Do list","Digital clock","Colors game"]
+let projectNames = ["WeatherHere app", "DuckDuckGo recreate","Cat vs Dogs","Blackjack game","Score counter","Charecters counter","Multiplication quiz","Calculator","Gaming quiz","Password generator","To-Do list","Digital clock","Colors game"]
 let apProjectsEl = document.querySelector(".ap-projects")
 
 for (let i in projectNames) {
