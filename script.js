f3d017bc3eb8258009aaf53be1eb83696ea0abd3
HaidarKhalid <HaidarKhalid01@gmail.com>
2023-04-06 15:56:29 +0300

    added json file, and copyRight

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let myProjects;
+let projectNames;
+async function getProjects() {
+    let response = await fetch('projects.json')
+    let data = await response.json()
+    myProjects = data['myProjects']
+    projectNames = data['projectNames']
+}
+getProjects()
+
 let menu = document.querySelector(".landing-nav-menu")
 let navLeft = document.querySelector(".nav-left")
 
@@ -44,22 +54,6 @@ window.onscroll = function (e) {
 }
 
 // projects image slider
-let myProjects = {
-    "DuckDuckGo recreate":{src:"images/projects/DuckDuckGo-recreate.png",link:"https://haidarkhalid.github.io/DuckDuckGo-recreate/"},
-    "WeatherHere app": {src:"images/projects/weatherhere-app.png",link:"https://haidarkhalid.github.io/WeatherHere/"},
-    "Cat vs Dogs":{src:"images/projects/cat-vs-dogs.png",link:"https://haidarkhalid.github.io/cat-vs-dogs/"},
-    "Blackjack game":{src:"images/projects/BlackJack.png",link:"https://haidarkhalid.github.io/blackjack-game/"},
-    "Score counter": {src:"images/projects/score-counter.png",link:"https://haidarkhalid.github.io/score-counter-new/"},
-    "Charecters counter": {src:"images/projects/charecter-couner.png",link:"https://haidarkhalid.github.io/charecters-counter/"},
-    "Multiplication quiz": {src:"images/projects/multiplication-quiz.png",link:"https://haidarkhalid.github.io/multiplication-quiz/"},
-    "Calculator": {src:"images/projects/calculator.png",link:"https://haidarkhalid.github.io/calculator/"},
-    "Gaming quiz": {src:"images/projects/Gaming-quiz.png",link:"https://haidarkhalid.github.io/Gaming-quiz/"},
-    "Password generator": {src:"images/projects/password-gene.png",link:"https://haidarkhalid.github.io/password-generator/"},
-    "To-Do list": {src:"images/projects/to-do-list.png",link:"https://haidarkhalid.github.io/to-do-list/"},
-    "Digital clock": {src:"images/projects/digital-clock.png",link:"https://haidarkhalid.github.io/Digital-clock/"},
-    "Colors game": {src:"images/projects/colors-game.png",link:"https://haidarkhalid.github.io/colors-game/"},
-}
-let projectNames = ["DuckDuckGo recreate","WeatherHere app","Cat vs Dogs","Blackjack game","Score counter","Charecters counter","Multiplication quiz","Calculator","Gaming quiz","Password generator","To-Do list","Digital clock","Colors game"]
 let x = 0
 function next() {
     x++
